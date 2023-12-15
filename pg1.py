926619e3156ed24c37b86f6393739e2935b68b59
sharmila-53 <153826457+sharmila-53@users.noreply.github.com>
2023-12-15 12:15:20 +0530

    Create pg1.py

---
+++ pg1.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(65+n-j),end=" ")
+    print()
+for i in range(1,n):
+    for j in range(1,n-i+1):
+        print(chr(65+n-j),end=" ")
+    print()
